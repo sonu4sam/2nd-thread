24ffa803dbb7910a3e9eaf5de833850de2ca3379
sonu4sam <sonu4sam@outlook.com>
2023-07-12 10:41:34 -0500

    css bugs

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BlogPostView, BlogDetailView # new
+from .views import BlogPostView, BlogDetailView, BlogCreateView # new
 
 urlpatterns = [
     path('', BlogPostView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # new
+    path('post/new/', BlogCreateView.as_view(), name='post_new') # new
 ]
