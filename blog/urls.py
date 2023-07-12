25c88976328cf57cb53c453ae0fb86dbd32188c5
sonu4sam <sonu4sam@outlook.com>
2023-07-12 08:48:42 -0500

    debug errors

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BlogPostView
+from .views import BlogPostView, BlogDetailView # new
 
 urlpatterns = [
     path('', BlogPostView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # new
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 class BlogPostView(ListView):
     model = Post
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post-detail.html'
